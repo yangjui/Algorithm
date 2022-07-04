b3a26557cc7faf72e407df1e589868651b57c806
Yang JuI <71171290+yangjui@users.noreply.github.com>
2022-07-04 19:53:28 +0900

    Update 1로 만들기.java

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -10,9 +10,9 @@ class Main
 		cnt[1] = 0;
 		
 		for(int i = 2; i < N+1; i++) {
-			cnt[i] = cnt[i-1] + 1;
-			if(i%3 == 0)
-				cnt[i] = Math.min(cnt[i], cnt[i/3] + 1);
+			cnt[i] = cnt[i-1] + 1;  //if문에 들어가지 못하면, i보다 1 작은 수의 cnt보다 1만큼 큰 cnt가 된다. --> i-1
+			if(i%3 == 0)  //만약 i=9라면! i=3일때 3을 한번 곱해줘서 만들 수 있음.(cnt+1)
+				cnt[i] = Math.min(cnt[i], cnt[i/3] + 1);  //i=3일때의 횟수보다 1 크다
 			if(i%2 == 0)
 				cnt[i] = Math.min(cnt[i], cnt[i/2] + 1);
 		}
